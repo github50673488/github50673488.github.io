3928fe26eda3c295f59ea11732a9489ddd94fc13
liuguojunjp <liuguojunjp@gmail.com>
2019-06-21 18:11:31 +0900

    commit 20190621-1811

--- bootstrap/20190130/05_abbr.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html><html><head>
-        <title>Bootstrap 实例 - 缩写</title>
-       <link rel="stylesheet" href="//apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css">  
-        <script src="//apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
-        <script src="//apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <body><abbr title="World Wide Web">WWW</abbr><br><abbr title="Real Simple Syndication" class="initialism">RSS</abbr></body></html>
--- bootstrap/20190206/34-Breadcrumbs.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html><html><head>
-   <title>Bootstrap 实例 - 面包屑导航</title>
-   <link href="//cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-   <script src="//cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-   <script src="//cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script></head><body><ol class="breadcrumb">
-  <li><a href="#">Home</a></li>
-  <li><a href="#">2013</a></li>
-  <li class="active">十一月</li></ol></body></html>
